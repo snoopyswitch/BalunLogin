67f40fc42ce52dea11a06ff372d1f65a1608cfb6
Mladen Vidovic <snoopyswitch@gmail.com>
2016-03-05 23:19:17 +0100

    change database

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 //will hold our database connection settings
 module.exports = {
-    'url' : 'postgres://pdmltpqzbpikuu:kCi1THR5ckBGVmISvCpSo3yENy@ec2-107-20-148-211.compute-1.amazonaws.com:5432/dchtssju7omka6'
+    'url' : 'mongodb://<dbuser>:<dbpassword>@ds023468.mlab.com:23468/bocadata'
 };
