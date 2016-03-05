19304562951754f6d28a3cf7f0757b0a1182dc56
Mladen Vidovic <snoopyswitch@gmail.com>
2016-03-05 11:26:34 +0100

    new

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 //will hold our database connection settings
 module.exports = {
-    'url' : 'mongodb://localhost/MyDatabase'
+    'url' : 'postgres://pdmltpqzbpikuu:kCi1THR5ckBGVmISvCpSo3yENy@ec2-107-20-148-211.compute-1.amazonaws.com:5432/dchtssju7omka6'
 };
